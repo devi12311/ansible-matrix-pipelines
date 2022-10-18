4824100653a5785a3f588567c1a0f1c50ac40f6c
devisarapi <darapi@rpay.ai>
2022-10-18 23:53:16 +0200

    wip

--- configure_domain_variables.sh
+++ configure_domain_variables.sh
@@ -1,8 +1,8 @@
 printf "Adding admin and proxy configuration \n"
 
-echo "matrix_domain: $1" >> ./inventory/host_vars/matrix.chatner.ch/vars.yml
-echo "matrix_homeserver_implementation: $2" >> ./inventory/host_vars/matrix.chatner.ch/vars.yml
-echo "matrix_homeserver_generic_secret_key: \"$3\"" >> ./inventory/host_vars/matrix.chatner.ch/vars.yml
-echo "matrix_ssl_lets_encrypt_support_email: \"$4\"" >> ./inventory/host_vars/matrix.chatner.ch/vars.yml
+echo "matrix_domain: $1" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
+echo "matrix_homeserver_implementation: $2" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
+echo "matrix_homeserver_generic_secret_key: \"$3\"" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
+echo "matrix_ssl_lets_encrypt_support_email: \"$4\"" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
 
 printf "DONE! \n"
