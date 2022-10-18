2b0b19227ee916bd2701edd382fffad40d02071b
devisarapi <darapi@rpay.ai>
2022-10-19 00:00:54 +0200

    wip

--- configure_database_variables.sh
+++ configure_database_variables.sh
@@ -1,9 +1,9 @@
 printf "Adding database configuration \n"
 
 echo "matrix_postgres_enabled: $1" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
-echo "matrix_synapse_database_host: \"$2\"" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
-echo "matrix_synapse_database_user: \"$3\"" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
-echo "matrix_synapse_database_password: \"$4\"" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
-echo "matrix_synapse_database_database: \"$5\"" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
+echo "matrix_synapse_database_host: $2" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
+echo "matrix_synapse_database_user: $3" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
+echo "matrix_synapse_database_password: $4" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
+echo "matrix_synapse_database_database: $5" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
 
 printf "DONE! \n"
--- configure_whatsapp_variables.sh
+++ configure_whatsapp_variables.sh
@@ -2,7 +2,7 @@ printf "Adding whatsapp configuration \n"
 
 echo "matrix_mautrix_whatsapp_enabled: $1" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
 echo "matrix_mautrix_whatsapp_container_image_self_build: $2" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
-echo "matrix_mautrix_whatsapp_container_image_self_build_repo: \"$3\"" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
-echo "matrix_mautrix_whatsapp_container_image_self_build_branch: \"$4\"" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
+echo "matrix_mautrix_whatsapp_container_image_self_build_repo: $3" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
+echo "matrix_mautrix_whatsapp_container_image_self_build_branch: $4" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
 
 printf "DONE!\n"
