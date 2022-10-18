0ab7591739ac516ac093f7668d22dd90bf999294
devisarapi <darapi@rpay.ai>
2022-10-18 23:13:29 +0200

    wip

--- configure_domain_variables.sh
+++ configure_domain_variables.sh
@@ -2,7 +2,7 @@ printf "Adding admin and proxy configuration \n"
 
 echo "matrix_domain: $1" >> ./inventory/host_vars/matrix.chatner.ch/vars.yml
 echo "matrix_homeserver_implementation: $2" >> ./inventory/host_vars/matrix.chatner.ch/vars.yml
-echo "matrix_homeserver_generic_secret_key: '$3'" >> ./inventory/host_vars/matrix.chatner.ch/vars.yml
-echo "matrix_ssl_lets_encrypt_support_email: '$4'" >> ./inventory/host_vars/matrix.chatner.ch/vars.yml
+echo "matrix_homeserver_generic_secret_key: \"$3\"" >> ./inventory/host_vars/matrix.chatner.ch/vars.yml
+echo "matrix_ssl_lets_encrypt_support_email: \"$4\"" >> ./inventory/host_vars/matrix.chatner.ch/vars.yml
 
 printf "DONE! \n"
--- configure_proxy_and_admin_variables.sh
+++ configure_proxy_and_admin_variables.sh
@@ -3,6 +3,6 @@ printf "Adding admin and proxy configuration \n"
 echo "matrix_nginx_proxy_base_domain_serving_enabled: $1" >> ./inventory/host_vars/matrix.chatner.ch/vars.yml
 echo "matrix_synapse_admin_enabled: $2" >> ./inventory/host_vars/matrix.chatner.ch/vars.yml
 echo "matrix_synapse_ext_password_provider_shared_secret_auth_enabled: $3" >> ./inventory/host_vars/matrix.chatner.ch/vars.yml
-echo "matrix_synapse_ext_password_provider_shared_secret_auth_shared_secret: '$4'" >> ./inventory/host_vars/matrix.chatner.ch/vars.yml
+echo "matrix_synapse_ext_password_provider_shared_secret_auth_shared_secret: \"$4\"" >> ./inventory/host_vars/matrix.chatner.ch/vars.yml
 
 printf "DONE!\n"
