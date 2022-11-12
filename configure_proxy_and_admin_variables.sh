1ec0aad3003dbf6a2bcc5111ddb1c6e4ff7bafa4
devisarapi <darapi@rpay.ai>
2022-11-12 16:47:35 +0100

    removed db aditional config

--- configure_proxy_and_admin_variables.sh
+++ configure_proxy_and_admin_variables.sh
@@ -1,8 +1,8 @@
 printf "Adding admin and proxy configuration \n"
 
-echo "matrix_nginx_proxy_base_domain_serving_enabled: $1" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
-echo "matrix_synapse_admin_enabled: $2" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
-echo "matrix_synapse_ext_password_provider_shared_secret_auth_enabled: $3" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
-echo "matrix_synapse_ext_password_provider_shared_secret_auth_shared_secret: $4" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
+echo "matrix_nginx_proxy_base_domain_serving_enabled: $1" >> ./inventory/host_vars/matrix.chatnerservices.ch/vars.yml
+echo "matrix_synapse_admin_enabled: $2" >> ./inventory/host_vars/matrix.chatnerservices.ch/vars.yml
+echo "matrix_synapse_ext_password_provider_shared_secret_auth_enabled: $3" >> ./inventory/host_vars/matrix.chatnerservices.ch/vars.yml
+echo "matrix_synapse_ext_password_provider_shared_secret_auth_shared_secret: $4" >> ./inventory/host_vars/matrix.chatnerservices.ch/vars.yml
 
 printf "DONE!\n"
--- configure_whatsapp_variables.sh
+++ configure_whatsapp_variables.sh
@@ -1,8 +1,8 @@
 printf "Adding whatsapp configuration \n"
 
-echo "matrix_mautrix_whatsapp_enabled: $1" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
-echo "matrix_mautrix_whatsapp_container_image_self_build: $2" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
-echo "matrix_mautrix_whatsapp_container_image_self_build_repo: $3" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
-echo "matrix_mautrix_whatsapp_container_image_self_build_branch: $4" >> ./inventory/host_vars/matrix.chatner.app/vars.yml
+echo "matrix_mautrix_whatsapp_enabled: $1" >> ./inventory/host_vars/matrix.chatnerservices.ch/vars.yml
+echo "matrix_mautrix_whatsapp_container_image_self_build: $2" >> ./inventory/host_vars/matrix.chatnerservices.ch/vars.yml
+echo "matrix_mautrix_whatsapp_container_image_self_build_repo: $3" >> ./inventory/host_vars/matrix.chatnerservices.ch/vars.yml
+echo "matrix_mautrix_whatsapp_container_image_self_build_branch: $4" >> ./inventory/host_vars/matrix.chatnerservices.ch/vars.yml
 
 printf "DONE!\n"
